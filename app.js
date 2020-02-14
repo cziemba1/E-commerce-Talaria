3b30d19d17eaa697eef4090ae6dcf475904ebdbd
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-02-14 00:18:54 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const logo = document.querySelector("#logo");
+const logIn = document.querySelector(".log-in-button");
+const modal = document.querySelector(".modal-bg");
+const modalClose = document.querySelector(".modal-close");
+
+console.log(logIn);
 
 logo.addEventListener("mouseover", changeNewLogo);
 logo.addEventListener("mouseleave", changeOldLogo);
+logIn.addEventListener("click", modalVisi);
+modalClose.addEventListener("click", modalCl);
 
 function changeNewLogo() {
     logo.src = "assets/logo-hover.png";
@@ -10,3 +17,14 @@ function changeNewLogo() {
 function changeOldLogo() {
     logo.src = "assets/logo.png";
 }
+
+function modalVisi() {
+    modal.classList.add("bg-active");
+}
+
+function modalCl() {
+    modal.classList.remove("bg-active");
+}
+
+
+
